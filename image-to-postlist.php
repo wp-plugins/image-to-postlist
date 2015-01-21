405ca7bd96ee2443a420ee476fd1fb96fbb550b8
jondor <jondor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-21 21:49:19 +0000

    minor fixes, clickable images
    git-svn-id: https://plugins.svn.wordpress.org/image-to-postlist/trunk@1072915 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- image-to-postlist.php
+++ image-to-postlist.php
@@ -1,36 +1,59 @@
 <?php
 /**
  * @package image-to-postlist
- * @version 1.4
+ * @version 1.5
  */
 /*
 Plugin Name: image to postlist
 Plugin URI: http://www.funsite.eu/plugins/image-to-postlist
 Description: This plugin adds the featured image icon to the posts- and pageslists.
 Author: Gerhard Hoogterp
-Version: 1.4
+Version: 1.5
 Author URI: http://www.funsite.eu/
+Text Domain: image-to-postlist
+Domain Path: /languages
 */
 
+if (!class_exists('basic_plugin_class')) {
+	require(plugin_dir_path(__FILE__).'basics/basic_plugin.class');
+}
 
+class image_to_postlist_class extends basic_plugin_class {
 
-class image_to_postlist_class {
+	function getPluginBaseName() { return plugin_basename(__FILE__); }
+	function getChildClassName() { return get_class($this); }
 
-	const FS_TEXTDOMAIN = 'image-to-postlist';
-	const FS_PLUGINNAME = 'image-to-postlist';
+	public function __construct() {
+		parent::__construct();
 
-	public $posttypelist = array('posts','pages');
-	
-    public function __construct() {
-		add_action('init', array($this,'myTextDomain'));
-		add_filter('plugin_row_meta', array($this,'image_to_postlist_PluginLinks'),10,2);
-
+		$post_types = get_post_types( array('public'   => true,'_builtin' => false), 'names', 'and');
+		$this->posttypelist=array_merge($this->posttypelist,$post_types);
+		
 		foreach($this->posttypelist as $posttype) {
 			add_filter('manage_'.$posttype.'_columns', array($this,'my_columns'));
 			add_action('manage_'.$posttype.'_custom_column',  array($this,'my_show_columns'));
 		}
-}
-    
+	}
+
+	function pluginInfoRight($info) { 
+		print __("<h3>Extra custom posttypes found:</h3>",self::FS_TEXTDOMAIN);
+
+		$post_types = get_post_types( array('public'   => true,'_builtin' => false), 'names', 'and');
+		if (empty($post_types)) {
+			print __("None",self::FS_TEXTDOMAIN);
+		} else {
+			foreach($post_types as $t) {
+				print $t.' ';
+			}
+		}
+
+	}
+	
+	const FS_TEXTDOMAIN = 'image-to-postlist';
+	const FS_PLUGINNAME = 'image-to-postlist';
+
+	public $posttypelist = array('posts','pages');	
+	
     function array_insert_after($key, &$array, $new_key, $new_value) {
 		if (array_key_exists($key, $array)) {
 			$new = array();
@@ -47,39 +70,23 @@ class image_to_postlist_class {
 	}
 
 	function my_columns($columns) {
-		$columns = $this->array_insert_after('title',$columns,'image',__('Featured image',self::FS_TEXTDOMAIN));
+		$columns = $this->array_insert_after('title',$columns,'fs_image',__('Featured image',self::FS_TEXTDOMAIN));
 		return $columns;
 	}
 
 	function my_show_columns($name) {
 		global $post;
 		switch ($name) {
-			case 'image':
+			case 'fs_image':
 					$post_thumbnail_id = get_post_thumbnail_id( $post->ID );
 					if ($post_thumbnail_id) {
 						$thumb = wp_get_attachment_image_src( $post_thumbnail_id, 'thumbnail'); 
-						echo '<img src="'.$thumb[0].'" alt="" style="width:50px">';
+						echo '<a href="'.admin_url().'post.php?post='.$post->ID.'&action=edit">';
+						echo '<img src="'.$thumb[0].'" alt="" style="width:50px"></a>';
 					} else {
 						echo "--";
 					}
 		}
-	}
-
-
-	function myTextDomain() {
-		load_plugin_textdomain(
-			self::FS_TEXTDOMAIN,
-			false,
-			dirname(plugin_basename(__FILE__)).'/languages/'
-		);
-	}
-	
-	function image_to_postlist_PluginLinks($links, $file) {
-		$base = plugin_basename(__FILE__);
-		if ($file == $base) {
-						$links[] = '<a href="https://wordpress.org/support/view/plugin-reviews/'.self::FS_PLUGINNAME.'#postform">' . __('Please rate me.',self::FS_TEXTDOMAIN) . '</a>';
-		}
-		return $links;
 	}
 }
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: http://www.funsite.eu/image-to-postlist/
 Tags: widget,rss, per page, plugin
 Requires at least: 3.0.1
 Tested up to: 4.1
-Stable tag: 1.4
+Stable tag: 1.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -35,6 +35,11 @@ option. It's not always about userinterfaces and difficult sollutions..;)
 1. screendump
 
 == Changelog ==
+
+= 1.5 =
+Changed to my basic class framework
+made the imagec clickable too
+Added basic support for custom posttypes. 
 
 = 1.4 =
 code cleanup
